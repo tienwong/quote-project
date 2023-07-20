bbe35dbb6f0075f55aac8431f3614fb586961781
Tien <45297291+tienwong@users.noreply.github.com>
2023-07-19 21:16:30 -0500

    Implemented basic quote routes and quote landing page

--- server/routes/quotes.js
+++ server/routes/quotes.js
@@ -3,7 +3,19 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.send('GET route on /quotes')
+    res.render('quotesLandingPage')
+})
+
+router.get('/addNewQuote', (req, res) => {
+    res.send('Add New Quote endpoint')
+})
+
+router.get('/byAuthor', (req, res) => {
+    res.send('By Author endpoint')
+})
+
+router.get('/byAuthor/:author', (req, res) => {
+    res.send('Quotes by the author ' + req.params.author)
 })
 
 router.post('/', (req, res) => {
