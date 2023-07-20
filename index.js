df98e3ed55597950fa1f7f8a6840618d2ab483be
Tien <45297291+tienwong@users.noreply.github.com>
2023-07-19 20:10:16 -0500

    Set up database logic and what is needed to connect to it

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
+require('dotenv').config()
+const database = require('./config/database')
 
 const app = express()
 
 app.get('/', (req, res) => {
+    console.log(database)
     res.send('Hello world!')
 })
 
